e51c2d5b847a480aed437900fa01b2175c24810e
Khizr_Sobirov <khizrsobirov@gmail.com>
2024-02-20 17:51:22 +0500

    Users modeli bo'ldi

--- crudSmall/Models/User.cs
+++ crudSmall/Models/User.cs
@@ -3,5 +3,9 @@
     public class User
     {
         public int Id { get; set; }
+        public string Name { get; set; } = "";
+        public string Surname { get; set; } = "";
+        public string Phone { get; set; } = "";
+
     }
 }
