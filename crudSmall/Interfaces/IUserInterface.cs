488abb27149b6f1de21a19a7c4094776cd5b5dbd
abror4ikdev <abror4ikdev@gmail.com>
2024-02-21 16:28:27 +0500

    khgkihbjhbvjh

---
+++ crudSmall/Interfaces/IUserInterface.cs
@@ -0,0 +1,13 @@
+﻿using crudSmall.Models;
+
+namespace crudSmall.Interfaces
+{
+    public interface IUserInterface
+    {
+        public List<User> GetAll();
+        public User GetById(int id);
+        public void Add(User user);
+        public void Delete(User user);
+        public void Update(User user);
+    }
+}
---
+++ crudSmall/Repositorys/UserService.cs
@@ -0,0 +1,40 @@
+﻿using crudSmall.Data;
+using crudSmall.Interfaces;
+using crudSmall.Models;
+
+namespace crudSmall.Repositorys
+{
+    public class UserService(AppDbContext dbContext)
+        : IUserInterface
+    {
+        private readonly AppDbContext _dbContext = dbContext;
+
+        public void Add(User user)
+        {
+            _dbContext.Users.Add(user);
+            _dbContext.SaveChanges();
+        }
+
+        public void Delete(User user)
+        {
+            _dbContext.Users.Remove(user);
+            _dbContext.SaveChanges();
+        }
+
+        public List<User> GetAll()
+        {
+            List<User> users = _dbContext.Users.ToList();
+            return users;
+        }
+
+        public User GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(User user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
